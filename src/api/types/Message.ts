4749b60b35fd0469eefe8172dacf3aa99c1bc472
Filip Sodić <filip.sodic@fer.hr>
2020-07-19 00:19:06 +0200

    fix typos

--- src/api/types/Message.ts
+++ src/api/types/Message.ts
@@ -1,9 +1,9 @@
 const enum Message {
 	Success = 'Success',
 	NoChanges = 'No changes were made',
-	MissingArticleName = 'No article name spacified',
+	MissingArticleName = 'No article with the specified name',
 	InvalidArticleName = 'The article with the given name does not exist',
-	UnexpectedError = 'An unexpected error occured',
+	UnexpectedError = 'An unexpected error occurred',
 }
 
 export default Message;
