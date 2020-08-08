80518e0a824e75e5f59f7890ec0bbda744244995
Filip Sodić <filip.sodic@fer.hr>
2020-08-08 20:48:54 +0200

    update environment configuration

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
 export default {
 	port: parseInt(process.env.SERVER_PORT || '8080', 10),
 };
---
+++ uninstall.sh
@@ -0,0 +1,3 @@
+sudo systemctl stop daily-article.service
+sudo systemctl disable daily-article.service
+sudo rm /etc/systemd/system/daily-article.service
